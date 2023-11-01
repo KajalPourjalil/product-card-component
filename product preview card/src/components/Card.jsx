c3fafa297b88787e281bb2e82e3f6784bd8ebe70
KajalPourjalil <rina.skyler@yahoo.com>
2023-11-01 22:10:54 +0300

    updates

--- product preview card/src/components/Card.jsx
+++ product preview card/src/components/Card.jsx
@@ -5,9 +5,20 @@ class Card extends React.Component {
     render() {
         return <div className="background">
           <div className="container">
-            
+          <div className="image"></div>
+          <div className="writings">
+          <h3 className="perfume">
+            PERFUME
+          </h3>
+          <h1 className="title">
+            Gabrielle Essence Eau De Parfum
+          </h1>
+          <p className="paragraph">
+            A floral, solar and voluptuous interpretation composed by Oliver
+            Polge, Perfumer-Creator for the House of CHANEL
+          </p>
           </div>
-
+          </div>
         </div>
     }
 }
